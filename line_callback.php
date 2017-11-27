1025f7bf37fa38e278d8ce0c5a68fa0bd8f818cc
nisidagod <32625692+nisidagod@users.noreply.github.com>
2017-11-27 14:39:41 +0900

    Update line_callback.php

--- line_callback.php
+++ line_callback.php
@@ -26,8 +26,8 @@ if (isset($_GET['code'])) {
   $url = 'https://api.line.me/v2/oauth/accessToken';
   $data = array(
     'grant_type' => 'authorization_code',
-    'client_id' => getenv('LOGIN_CHANNEL_ID'),
-    'client_secret' => getenv('LOGIN_CHANNEL_SECRET'),
+    'client_id' => getenv('1539757035'),
+    'client_secret' => getenv('45d7f699c88dcb8e01fca97fd09a22f1'),
     'code' => $_GET['code'],
     'redirect_uri' => $callback
   );
@@ -59,8 +59,8 @@ if (isset($_GET['code'])) {
     echo '<p>displayName : ' . htmlspecialchars($profile["displayName"], ENT_QUOTES) . '</p>';
     echo '<p>userId : ' . htmlspecialchars($profile["userId"], ENT_QUOTES) . '</p>';
 
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('xRqejpxMVQK8pUA17z8jMKKDaEKmis3XoYArpP+EkrUrhwO7RSb10BCo3IpfcAJoAAxqcvcwrHH0INAXfrMee0+OePxm2umiq+k4SdS05O3OcLW/FN4RLqhdrTQ6DC5XZvgn3UaUmMYqLUouPfvpUQdB04t89/1O/w1cDnyilFU='));
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('329695d93f519ef5bdc856f2276c7b4d')]);
     $bot->pushMessage($profile["userId"], new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Logged in via LINE Login v2.'));
   }
 }
